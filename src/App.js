cd3137366cf935c14a3d7b954c29a00b8bde1f84
midhulsasikumar-an <midhulsasikumar.in@gmail.com>
2026-03-01 17:20:45 +0530

    Removed backend folder from frontend repository and connected Both

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     setPrediction('');
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://your-render-url.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
